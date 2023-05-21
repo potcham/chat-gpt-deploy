ff9b72f2d90dd5029201a8c2c7b358ba85245812
potcham <bcerronr@gmail.com>
2023-05-21 00:04:14 -0400

    request chat file

---
+++ chat_gpt.py
@@ -0,0 +1,24 @@
+import os 
+import openai
+from credentials import OPENAI_API_KEY, OPENAI_ORG_ID
+
+openai.organization = OPENAI_ORG_ID
+openai.api_key = OPENAI_API_KEY
+
+# model = openai.Model.retrieve('gpt-3.5-turbo')
+
+while True:
+    prompt = input('\nIntroduce una pregunta: ')
+
+    if prompt == 'salir':
+        break
+    completion = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {'role':'user',
+            'content': prompt}
+        ],
+        max_tokens = 2048,
+    )
+
+    print(completion.choices[0].message.content)
