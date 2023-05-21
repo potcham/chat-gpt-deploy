f0d94c7bf76979965710f3d110bb5961511a4785
potcham <bcerronr@gmail.com>
2023-05-21 02:00:50 -0400

    adding typer

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # chat-gpt-deploy
+
+1. Set up environment
+
+```bash
+conda create -n openai python=3.9
+```
+2. activate the conda environment
+```bash
+conda activate opeani
+```
+3. install official python binding for openai
+```bash
+pip install openai
+```
+
+4. install typer for better terminal
+
+```bash
+pip install "typer[all]"
+```
--- main.py
+++ main.py
@@ -1,33 +1,65 @@
 import openai
 from credentials import OPENAI_API_KEY, OPENAI_ORG_ID
+import typer
+from rich import print
+from rich.table import Table 
 
-openai.organization = OPENAI_ORG_ID
-openai.api_key = OPENAI_API_KEY
+def main():
 
-## 1. Contexto para el asistente
-messages = [{'role':'system', 
-             'content':'Eres un asistente de traducciones de idiomas'}]
+    openai.organization = OPENAI_ORG_ID
+    openai.api_key = OPENAI_API_KEY
 
-## 2. Inicio de interacción
-while True:
+    print('\n🔥 [bold green]Asistente Virtual con ChatGPT[/bold green] 🔥')
 
-    prompt = input('\n¿Cual es tu consulta?: ')
+    table = Table('Comando', 'Descripcion')
+    table.add_row('exit', 'Finalizar conversación')
+    table.add_row('new', 'Iniciar nueva conversación')
 
-    if prompt == 'salir':
-        break
+    print(table)
 
-    ## 3. Nueva consulta para el modelo
-    messages.append({'role':'user', 'content': prompt})
+    ## 1. Contexto para el asistente
+    context = {'role':'system', 
+                'content':'Eres un asistente de traducciones de idiomas'}
+    messages = [context]
 
-    response = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
-        messages=messages,
-        max_tokens = 2048,
-    )
+    ## 2. Inicio de interacción
+    while True:
 
-    # 4. Guardamos el historial de la conversacion, nuevas consultas tendran una referencia
-    response_content = response.choices[0].message.content
+        prompt = __prompt()
 
-    messages.append({'role':'assistant', 'content': response_content})
+        if prompt == 'new':
+            print('\n🆕Nueva conversación')
+            messages = [context]
+            prompt = __prompt()
 
-    print(response_content)
+        ## 3. Nueva consulta para el modelo
+        messages.append({'role':'user', 'content': prompt})
+
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=messages,
+            max_tokens = 1024,
+        )
+
+        # 4. Guardamos el historial de la conversacion, nuevas consultas tendran una referencia
+        response_content = response.choices[0].message.content
+
+        messages.append({'role':'assistant', 'content': response_content})
+
+        print(f'[bold green]>[/bold green] [green]{response_content}[/green]')
+
+def __prompt()->str:
+    prompt = typer.prompt('\n💬¿Cual es tu consulta?: ')
+     
+    if prompt == 'exit':
+        exit = typer.confirm('🔥 ¿Estas seguro?')
+        if exit:
+            print('👋 ¡Hasta luego!')
+            raise typer.Abort
+        
+        return __prompt()
+    
+    return prompt
+
+if __name__ == "__main__":
+    typer.run(main)
